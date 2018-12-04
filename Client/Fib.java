6002da140a0d960e343f83dc69fb8d032fd5dfb2
MatRabPC <matthew.rabski@uoit.net>
2018-12-04 17:44:11 -0500

    Removing old files

--- Client/Fib.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-import java.util.ArrayList;
-
-public interface Fib extends Remote {
-   public final static String SERVICENAME="FibService";
-   public String fileChooseReturnPath() throws RemoteException;   
-   public String dirChooseReturnPath() throws RemoteException;
-   public ArrayList<String> mp3LookUp(String name) throws RemoteException;
-}
--- Client/mp3agic/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"com.mpatric.mp3agic"}]
