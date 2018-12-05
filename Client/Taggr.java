9ca90af22e41810975937393003e9f5b386e3e29
MatRabPC <matthew.rabski@uoit.net>
2018-12-04 23:29:00 -0500

    Removing files

--- Client/Taggr.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-import java.util.ArrayList;
-
-public interface Taggr extends Remote {
-   public final static String SERVICENAME="TaggrService";
-   public ArrayList<String> mp3LookUp(String name) throws RemoteException;   
-}
